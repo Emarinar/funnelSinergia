3a044264a48fe38636466d28996fb8e1dbfd373c
emarinar <eduardo.marin.1990@hotmail.com>
2025-03-01 21:02:40 -0500

    Actualización: Secciones de css y netlify

--- netlify/submitForm.js
+++ netlify/submitForm.js
@@ -48,3 +48,10 @@ exports.handler = async (event, context) => {
     };
   }
 };
+
+fetch('/.netlify/functions/submitForm', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(data)
+});
+
